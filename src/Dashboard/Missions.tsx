ed87241628fa1812b831fe096d87cdfea84c9457
Mathieu <mathieu.dumontier@oclock.io>
2022-03-22 15:52:20 +0100

    Update pub

--- src/Dashboard/Missions.tsx
+++ src/Dashboard/Missions.tsx
@@ -3,25 +3,15 @@ import { Box, Fab, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { useAuth } from '../contexts/AuthProvider';
 
+import lsmicGotTalent from './LSMIC_TALENT.png';
+
 const Missions = () => {
     const auth = useAuth();
 
     return (
-        <Box sx={{ position: 'relative', width: "500px", height: "500px", bgcolor: 'background.default' }}>
-            {(auth.user.isAdmin && false) && (
-                <Fab size={"small"} color="primary" aria-label="add" sx={{ position: 'absolute', bottom: '5px', right: '5px' }}>
-                    <AddIcon />
-                </Fab>
-            )}
-            <Box sx={{ marginBottom: '50px' }}>
-                <Typography variant={"h6"} sx={{ color: 'text.primary' }}>Missions</Typography>
-                <Typography variant={"subtitle1"} sx={{ color: 'text.primary' }}>Cette fonctionnalité n'est pas encore disponnible</Typography>
-            </Box>
-
-            <iframe width="500" height="315" src="https://www.youtube.com/embed/rbWmOIdUEjw"
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen />
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'relative', width: "500px", height: "500px", bgcolor: 'background.default', padding: '10px', boxSizing: 'border-box' }}>
+            <img style={{ maxWidth: '100%', maxHeight: '90%' }} src={lsmicGotTalent} />
+            <a style={{ color: '#90caf9', marginTop: '5px' }} target="_blank" href="https://forms.gle/MHumvXMCBGfFePYY9">INSCRIPTION ICI</a>
         </Box>
     )
 }
